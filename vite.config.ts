e7170b640016c88fc37e3235f01df66803a26d36
Arie <rest7261@gmail.com>
2026-01-30 11:31:23 +0200

    Fix base for Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/crypto-coins-spa/",
+  base: "/", // Cloudflare Pages base
   plugins: [react()],
   server: { open: true },
 })
