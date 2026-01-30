3d34b3f767bc3fdfd207905a3f94d84417c62a2e
Arie <rest7261@gmail.com>
2026-01-30 13:27:34 +0200

    Set Vite base to root

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,9 @@
-ע
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: { open: true },
+  base: "/", // ✅ Cloudflare Pages
+});
+
